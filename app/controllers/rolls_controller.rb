675f1ad08cd6a3cf89306fba0412e63b4c4089f7
matthew kettelkamp <mattkettelkamp@gmail.com>
2022-12-23 15:30:45 +0800

    made some visual changes and made the path to the new camera creation actually a link_to. Now need to add the method in the controller as well

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -1,6 +1,7 @@
 class RollsController < ApplicationController
 
   def index
+    # @camera = Camera.find(params[:id])
     @user = current_user
     @rolls = Roll.all
     @users_rolls = current_user.rolls
