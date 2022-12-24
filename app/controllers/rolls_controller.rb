34d4d374483d7922770261e3f3515baabb74cb35
matthew kettelkamp <mattkettelkamp@gmail.com>
2022-12-24 14:34:34 +0800

    made it so in the controller we can grab the specific camera that is selected and get its rolls and not the total rolls for the user

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -1,9 +1,10 @@
 class RollsController < ApplicationController
 
   def index
-    # @camera = Camera.find(params[:id])
+    @camera = Camera.find(params[:camera_id]) # gets the camera with the id needed
+    @cameras_rolls = @camera.rolls # gets the rolls for that specific camera
     @user = current_user
     @rolls = Roll.all
-    @users_rolls = current_user.rolls
+    @users_rolls = current_user.rolls  # this would be all of the cameras rolls
   end
 end
