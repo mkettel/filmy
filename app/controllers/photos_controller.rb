f1669795df6809b39a955dad6b70a771bf4cd823
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-02-16 14:07:27 -0500

    stopping here for a few. still having trouble saving the frames to the database. May need to restructure some things

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,15 +2,30 @@ class PhotosController < ApplicationController
 
   #This should allow for the settings and details for the photo to be saved to the roll.
   def create
+    @photo = Photo.new(photo_params)
     @roll = Roll.find(params[:roll_id])
-    @frame_number = params[:frame_number]
-    @shutter_speed = params[:shutter_speed]
-    @aperture = params[:aperture]
-    @description = params[:description]
-    @photo = @roll.photos.build(photo_params)
+    @camera = Camera.find(params[:camera_id])
+    params[:frames] do |frame_params|
+      @roll.frames.create(
+        photo: frame_params[:photo],
+        shutter_speed: frame_params[:shutter_speed],
+        aperture: frame_params[:aperture],
+        description: frame_params[:description]
+      )
+    end
     if @photo.save
       raise
     end
+    # if params[:frame_number].present?
+    #   @frame_number = params[:frame_number]
+    #   @shutter_speed = params[:shutter_speed]
+    #   @aperture = params[:aperture]
+    #   @description = params[:description]
+    #   @photo = @roll.photos.build(photo_params)
+    #   if @photo.save
+    #     raise
+    #   end
+    # end
   end
 
 
