d126b9dda79e2b8f6a8416ddaa0252625fd5d472
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-02-17 17:56:08 -0500

    got a thimbnail to pop up on image load so that is a start to something good happening

--- app/javascript/controllers/frame_controller.js
+++ app/javascript/controllers/frame_controller.js
@@ -3,5 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="frame"
 export default class extends Controller {
   connect() {
+    const imageInput = document.getElementById('image');
+    const imagePreview = document.getElementById('image-preview');
+
+    imageInput.addEventListener('change', function() {
+      const file = this.files[0];
+      const reader = new FileReader();
+
+      reader.addEventListener('load', function() {
+        imagePreview.src = reader.result;
+        imagePreview.style.display = 'block';
+      });
+
+      reader.readAsDataURL(file);
+    });
   }
 }
