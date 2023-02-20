db7cd74e70e62f5f241c20607b5a28812088e1ba
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-02-20 16:39:34 -0500

    can now finally upload multiple thumbnail images. This i sdue to going through an array that is created and using the proper inde it uploads the correct image. Now need to add the ability to save the image to the backend and then also need to be able to delete an image and save to the backend also

--- app/javascript/controllers/frame_controller.js
+++ app/javascript/controllers/frame_controller.js
@@ -3,25 +3,28 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect() {
-    const input = document.querySelector("input");
-    const output = document.querySelector("output");
-    let imgArray = []
+    const input = document.querySelectorAll("input[type=file]");
+    const output = document.querySelectorAll("output");
+    let imgArray = Array.from(input).map(() => []);
 
-    input.addEventListener("change", function() {
-      const file = input.files
-      imgArray.push(file[0])
-      displayImages()
-    })
-
-    function displayImages() {
+    input.forEach((input, i) => {
+      input.addEventListener("change", function() {
+        const file = input.files;
+        imgArray[i].push(file[0]);
+        displayImages(i);
+      });
+    });
+    // for each file that is added to the array, it goes through and creates a litle URL for the image and adds the HTML to the images variable
+    // No URL at the moment because that breaks it
+    function displayImages(i) {
       let images = ""
-      imgArray.forEach((image, index) => {
+      imgArray[i].forEach((image, index) => {
         images += `<div class="image">
-        <img src="${URL.createObjectURL(image)}" alt="image">
+        <img src="#" alt="image">
         <span onClick="deleteImage(${index})">&times;</span>
         </div>`
       })
-      output.innerHTML = images
+      output[i].innerHTML = images;
     }
 
     function deleteImage(index) {
