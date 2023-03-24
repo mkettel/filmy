6e4487528bcc0820ea4ced738565d8f2f874f0f5
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-03-24 14:03:23 -0500

    i think that i have all of the changes but now i need to make sure the order does not change

--- app/controllers/frames_controller.rb
+++ app/controllers/frames_controller.rb
@@ -8,31 +8,54 @@ class FramesController < ApplicationController
   end
 
   def create
-    @roll = Roll.find(params[:roll_id])
-    @frame = @roll.frames.build(frame_params)
-    @camera = Camera.find(params[:camera_id])
-    @roll_id = @roll.id
+    @roll = Roll.find(params[:roll_id])  # finds the roll that the frame is being created for
+    @frame = @roll.frames.build(frame_params) # creates a new frame for the roll
+    @camera = Camera.find(params[:camera_id]) # finds the camera that the roll is associated with
+    @roll_id = @roll.id # sets the roll id to the roll id
     if @frame.save
       redirect_to camera_rolls_path(@camera, @roll)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @camera = Camera.find(params[:camera_id])
-    @roll = Roll.find(params[:roll_id])
-    @frame = Frame.find(params[:id])
+    @camera = Camera.find(params[:camera_id]) # finds the camera that the roll is associated with
+    @roll = Roll.find(params[:roll_id]) # finds the roll that the frame is being created for
+    @frame = Frame.find(params[:id]) # finds the frame that is being edited
   end
 
   def update
-    # @camera = current_user.cameras.find_by_id(params[:id])
-    # @camera = Camera.find(params[:camera_id])
-    # @roll = current_user.cameras.rolls.find_by_id(params[:roll_id])
-    @frame = Frame.find(params[:id])
-    if @frame.update(frame_params)
-      flash[:notice] = "your frame was updated"
-    end
-    # redirect_to frameshit_path(@frame)
+    @camera = Camera.find(params[:camera_id]) # finds the camera that the roll is associated with
+    @roll = Roll.find(params[:roll_id]) # finds the roll that the frame is being created for
+    @frame = Frame.find(params[:id]) # finds the frame that is being edited
 
+    if @frame.update(frame_params)
+      flash[:notice] = "Your frame was updated."
+
+      # Check which fields were changed
+      if @frame.description_changed?
+        # Do something if the description field was changed
+        @frame.update_attribute(:description, frame_params[:description])
+      end
+
+      if @frame.shutter_speed_changed?
+        # Do something if the shutter_speed field was changed
+        @frame.update_attribute(:shutter_speed, frame_params[:shutter_speed])
+      end
+
+      if @frame.aperture_changed?
+        # Do something if the aperture field was changed
+        @frame.update_attribute(:aperture, frame_params[:aperture])
+      end
+
+      if @frame.photo_changed?
+        # Do something if the photo field was changed
+        @frame.update_attribute(:photo, frame_params[:photo])
+      end
+
+      redirect_to frameshit_path(@frame) # redirect to the frame show page
+    end
   end
 
   private
--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -1,69 +1,131 @@
 import { Controller } from "@hotwired/stimulus"
 
-
-// UPDATES THE FIELDS IN THE FRAMES AJAX STYLE MFER
-
-// Connects to data-controller="editor"
 export default class extends Controller {
+
   connect() {
     console.log("howdy");
     // Get CSRF token from meta tag
     const csrfToken = document.querySelector("meta[name=csrf-token]").getAttribute("content");
 
-    const saveBtn = document.getElementById("save-btn");
-    saveBtn.addEventListener("click", function() {
+    const saveBtns = document.querySelectorAll("#save-btn"); // get all the save buttons
+    saveBtns.forEach(function(saveBtn) {                // loop through each save button
+      saveBtn.addEventListener("click", function() {    // add a click event listener to each save button
+        const rollId = document.querySelector(".photo-row-container").getAttribute("data-roll-id"); // get the roll id
+        console.log(rollId);
 
-      // Get all the editable fields
-      const editableFields = document.querySelectorAll("[contenteditable='true']");
+        const urlParts = window.location.pathname.split('/');
+        const cameraId = urlParts[2];
+        console.log(cameraId);
 
-      // Collect the changes made to each field
-      const changes = {};
-      editableFields.forEach(function(field) {
-        const key = field.classList[0];
-        const value = field.innerText;
-        changes[key] = value;
-      });
-      console.log(editableFields);
+        const frame = saveBtn.closest('.frame');
+        const frameId = frame.id;
+        console.log(frameId);
 
-      // Get the Roll ID (html element grab)
-      const rollId = document.querySelector(".photo-row-container").getAttribute("data-roll-id");
-      console.log(rollId);
+        const editableFields = frame.querySelectorAll("[contenteditable='true']");
+        const changes = {};
+        editableFields.forEach(function(field) {
+          const key = field.classList[0];
+          const value = field.innerText;
+          changes[key] = value;
+        });
 
-      // Get the camera ID (from url)
-      const urlParts = window.location.pathname.split('/');
-      const cameraId = urlParts[2]; // camera_id is the 3rd segment in the URL
-      console.log(cameraId);
+        const xhr = new XMLHttpRequest();
+        const url = `/cameras/${cameraId}/rolls/${rollId}/frames/${frameId}`
+        xhr.open("PATCH", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("X-CSRF-Token", csrfToken);
 
-      // Get the frame ID
-      const frameId = document.querySelector(".frame").id;
-      console.log(frameId);
+        const toast = document.querySelector(".toast");
+        toast.classList.remove('hidden');
+        toast.classList.add('show');
 
-      // Send the changes to the server
-      const xhr = new XMLHttpRequest();
-      const url = `/cameras/${cameraId}/rolls/${rollId}/frames/${frameId}`
-      xhr.open("PATCH", url, true);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.setRequestHeader("X-CSRF-Token", csrfToken); // Add CSRF token to header
+        setTimeout(function() {
+          toast.classList.add('hidden');
+        }, 2000);
 
-      // Show a toast notification
-      const toast = document.querySelector(".toast");
-      toast.classList.remove('hidden');
-      toast.classList.add('show');
-      // document.querySelector(".main-roll-container").appendChild(toast);
-
-      // Hide the toast after a short delay
-      setTimeout(function() {
-        toast.classList.add('hidden');
-      }, 2000);
-
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          // handle success
-
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            // handle success
+          };
         };
-      };
-      const data = { frame: changes };
-      xhr.send(JSON.stringify(data));
+        const data = { frame: changes };
+        xhr.send(JSON.stringify(data));
+      });
     });
   }
 }
+
+
+
+
+
+
+// import { Controller } from "@hotwired/stimulus"
+
+
+// // UPDATES THE FIELDS IN THE FRAMES AJAX STYLE MFER
+
+// // Connects to data-controller="editor"
+// export default class extends Controller {
+//   connect() {
+//     console.log("howdy");
+//     // Get CSRF token from meta tag
+//     const csrfToken = document.querySelector("meta[name=csrf-token]").getAttribute("content");
+
+//     const saveBtn = document.getElementById("save-btn");
+//     saveBtn.addEventListener("click", function() {
+
+//       // Get all the editable fields
+//       const editableFields = document.querySelectorAll("[contenteditable='true']");
+
+//       // Collect the changes made to each field
+//       const changes = {};
+//       editableFields.forEach(function(field) {
+//         const key = field.classList[0];
+//         const value = field.innerText;
+//         changes[key] = value;
+//       });
+//       console.log(editableFields);
+
+//       // Get the Roll ID (html element grab)
+//       const rollId = document.querySelector(".photo-row-container").getAttribute("data-roll-id");
+//       console.log(rollId);
+
+//       // Get the camera ID (from url)
+//       const urlParts = window.location.pathname.split('/');
+//       const cameraId = urlParts[2]; // camera_id is the 3rd segment in the URL
+//       console.log(cameraId);
+
+//       // Get the frame ID
+//       const frameId = document.querySelector(".frame").id;
+//       console.log(frameId);
+
+//       // Send the changes to the server
+//       const xhr = new XMLHttpRequest();
+//       const url = `/cameras/${cameraId}/rolls/${rollId}/frames/${frameId}`
+//       xhr.open("PATCH", url, true);
+//       xhr.setRequestHeader("Content-Type", "application/json");
+//       xhr.setRequestHeader("X-CSRF-Token", csrfToken); // Add CSRF token to header
+
+//       // Show a toast notification
+//       const toast = document.querySelector(".toast");
+//       toast.classList.remove('hidden');
+//       toast.classList.add('show');
+//       // document.querySelector(".main-roll-container").appendChild(toast);
+
+//       // Hide the toast after a short delay
+//       setTimeout(function() {
+//         toast.classList.add('hidden');
+//       }, 2000);
+
+//       xhr.onreadystatechange = function() {
+//         if (xhr.readyState === 4 && xhr.status === 200) {
+//           // handle success
+
+//         };
+//       };
+//       const data = { frame: changes };
+//       xhr.send(JSON.stringify(data));
+//     });
+//   }
+// }
